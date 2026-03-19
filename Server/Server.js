813ede4b571e221e3ea566acabce364af2b47529
Manju-S20 <s.manjushanmugasundaram@gmail.com>
2026-03-19 19:07:01 +0530

    Fixed CORS issue

--- Server/Server.js
+++ Server/Server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: "https://imagegenerator-tan.vercel.app/",
-    withCredentials: true
+    Credentials: true
 }));
 
 app.get('/', (req, res) => {
